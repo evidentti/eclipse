90a9d83a4e2d24e8983712023441d3cb23fe2f84
Hannu Honka <hannu.honka@tieto.com>
2017-03-31 06:24:58 +0300

    demo app added

--- dashboard/src/main/webapp/js/dashboard-app.js
+++
@@ -1,2 +0,0 @@
-
-angular.module("dashboardApp",['controllers']).config(function(){}).run(function($rootScope){$rootScope.test='RUN';});var controllers=angular.module('controllers',[]).controller('appController',function($rootScope){console.log('appController',$rootScope.test);});
