4e87ce5185d97895f9a423dc73d1aa591aa3b6f2
Hannu Honka <hannu.honka@tieto.com>
2017-04-01 18:05:06 +0300

    “backup”

--- demo/src/main/resources/static/js/details.controller.js
+++ demo/src/main/resources/static/js/details.controller.js
@@ -2,6 +2,7 @@ angular.module('details.controller', [])
 
 .controller('detailsController', function($rootScope, $scope, $state) {
 	console.log('detailsController', $rootScope.created);
+
 	
 	$scope.$on('$destroy', function() {
 		console.log('detailsController', $rootScope.destroyed);
