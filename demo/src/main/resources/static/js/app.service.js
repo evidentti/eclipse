e0bca1a0859405beec0925739154705617741afe
Hannu Honka <hannu.honka@tieto.com>
2017-04-02 06:17:45 +0300

    “backup”

--- demo/src/main/resources/static/js/app.service.js
+++ demo/src/main/resources/static/js/app.service.js
@@ -7,27 +7,57 @@ angular.module('app.service', ['ngResource'])
 		}
 	});
 }])
-.service('AppService', ['IssueResource', '$q', '$timeout', function (IssueResource, $q, $timeout) {
+.factory('CategoryResource', ['$resource', function ($resource) {
+	return $resource('https://dev.hel.fi:443/paatokset/v1/category/', {}, {
+		get: {
+			method: 'GET',
+			cache: false
+		}
+	});
+}])
+.service('AppService', ['CategoryResource', 'IssueResource', '$q', '$timeout', function (CategoryResource, IssueResource, $q, $timeout) {
 	var self = this;
-	self.objects = null;
+	
+	self.categories = null;
+	self.issues = null;
+	
+	self.getCategories = function(refresh) {
+		console.log('AppService.getCategories');
+		var deferred = $q.defer();
+        $timeout(function () {
+        	deferred.notify('started');
+        	if(refresh || !angular.isArray(self.categories)) {
+        		CategoryResource.get({}).$promise.then(function (response) {
+        			self.categories = response.objects;
+        			deferred.resolve(angular.copy(self.categories));
+            	}, function (error) {
+            		deferred.reject(error);
+            	});
+        	}
+        	else {
+        		deferred.resolve(angular.copy(self.categories));
+        	}       
+        }, 0);
+
+        return deferred.promise;
+	};
 	
 	self.getIssues = function(refresh) {
 		console.log('AppService.getIssues', refresh);
 		var deferred = $q.defer();
         $timeout(function () {
-        	if(refresh || !angular.isArray(self.objects)) {
+        	if(refresh || !angular.isArray(self.issues)) {
         		deferred.notify('started');
                 IssueResource.get({}).$promise.then(function (response) {
-                	self.objects = response.objects;
-        			deferred.resolve(angular.copy(self.objects));
+                	self.issues = response.objects;
+        			deferred.resolve(angular.copy(self.issues));
                 }, function (error) {
                     deferred.reject(error);
                 });
         	}
         	else {
-        		deferred.resolve(angular.copy(self.objects));
+        		deferred.resolve(angular.copy(self.issues));
         	}
-            
         }, 0);
 
         return deferred.promise;
--- demo/src/main/resources/static/js/main.controller.js
+++ demo/src/main/resources/static/js/main.controller.js
@@ -4,25 +4,63 @@ angular.module('main.controller', [])
 	console.log('mainController', $rootScope.created);
 	
 	var self = this;
-	self.loading = false;
-	self.objects = null;
 	
-	self.getIssues = function(refresh) {
-		AppService.getIssues(refresh).then(function (response) {
-			console.debug('READY', response);
-			self.objects = angular.isArray(response) ? response : [];
+	self.loadingCategories = false;
+	self.loadingIssues = false;
+	self.categories = null;
+	self.issues = null;
+	
+	self.selectedCategory = null;
+	
+	function createFilterFor(category) {
+		return function filterFn(field) {
+			return (angular.lowercase(field.name).indexOf(angular.lowercase(category)) === 0);
+		};
+	}
+	
+	self.getCategories = function(refresh) {
+		AppService.getCategories(refresh).then(function (response) {
+			console.debug('mainController.getCategories', response);
+			self.categories = angular.isArray(response) ? response : [];
         }, function (error) {
-        	console.error('ERROR', error);
+        	console.error('mainController.getCategories', error);
         }, function(notification) {
-        	console.debug('NOTIFY', notification);
-        	self.loading = true;
+        	console.debug('mainController.getCategories: notify', notification);
+        	self.loadingCategories = true;
         }).finally(function () {
-        	console.debug('FINALLY');
-        	self.loading = false;
+        	console.debug('mainController.getCategories: finally');
+        	self.loadingCategories = false;
         });
 	};
 	
-	self.getIssues();
+	self.getIssues = function(refresh) {
+		AppService.getIssues(refresh).then(function (response) {
+			console.debug('mainController.getIssues', response);
+			self.issues = angular.isArray(response) ? response : [];
+        }, function (error) {
+        	console.error('mainController.getIssues', error);
+        }, function(notification) {
+        	console.debug('mainController.getIssues: notify', notification);
+        	self.loadingIssues = true;
+        }).finally(function () {
+        	console.debug('mainController.getIssues: finally');
+        	self.loadingIssues = false;
+        });
+	};
+	
+	self.openIssue = function(issue) {
+		console.log('mainController.openIssue', issue);
+	};
+	
+	self.selectedCategoryChange = function(category) {
+		console.log('mainController.selectedCategoryChange', category);
+	};
+	
+	self.querySearch = function(query) {
+		return query ? self.categories.filter( createFilterFor(query) ) : self.categories;
+	};
+	
+	self.getCategories(true);
 	
 	$scope.$on('$destroy', function() {
 		console.log('mainController', $rootScope.destroyed);
