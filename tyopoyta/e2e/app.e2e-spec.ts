fc62f3d213462e81b6859792680fbb2e27c2a675
Hannu Honka <hannu.honka@tieto.com>
2017-06-22 01:19:54 +0300

    working base of työpöytä

---
+++ tyopoyta/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { TyopoytaPage } from './app.po';
+
+describe('tyopoyta App', () => {
+  let page: TyopoytaPage;
+
+  beforeEach(() => {
+    page = new TyopoytaPage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
