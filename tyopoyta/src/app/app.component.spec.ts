ea20aa1821a6b3d178f2c54aa1a576b7cf6c09be
Hannu Honka <hannu.honka@tieto.com>
2017-06-24 06:46:23 +0300

    “backup”

--- tyopoyta/src/app/app.component.spec.ts
+++ tyopoyta/src/app/app.component.spec.ts
@@ -33,10 +33,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Työpöytä');
   }));
 
-  //  it('should render title in a h1 tag', async(() => {
-  //    const fixture = TestBed.createComponent(AppComponent);
-  //    fixture.detectChanges();
-  //    const compiled = fixture.debugElement.nativeElement;
-  //    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  //  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#app-title').textContent).toContain('Työpöytä');
+  }));
 });
