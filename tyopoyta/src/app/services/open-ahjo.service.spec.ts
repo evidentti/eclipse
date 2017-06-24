449c7e5a297513c1aa75f5cc061a14232cbe5576
Hannu Honka <hannu.honka@tieto.com>
2017-06-24 05:13:24 +0300

    “backup”

--- tyopoyta/src/app/services/open-ahjo.service.spec.ts
+++ tyopoyta/src/app/services/open-ahjo.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { OpenAhjoService } from './open-ahjo.service';
 
 describe('OpenAhjoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [OpenAhjoService]
+      providers: [OpenAhjoService],
+      imports: [
+        HttpModule,
+        JsonpModule
+      ]
     });
   });
 
-//  it('should be created', inject([OpenAhjoService], (service: OpenAhjoService) => {
-//    expect(service).toBeTruthy();
-//  }));
+  it('should be created', inject([OpenAhjoService], (service: OpenAhjoService) => {
+    expect(service).toBeTruthy();
+  }));
 });
