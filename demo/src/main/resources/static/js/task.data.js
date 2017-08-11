7449022cddadd00a9b3033ee8d14cd0ebe7516d5
Hannu Honka <hannu.honka@tieto.com>
2017-08-11 11:20:38 +0300

    working example

--- demo/src/main/resources/static/js/task.data.js
+++ demo/src/main/resources/static/js/task.data.js
@@ -9,27 +9,16 @@ angular.module('task.service', ['ngResource'])
 .service('TaskService', ['localStorageService', '$http', '$q', '$timeout', function(localStorageService, $http, $q, $timeout) {
 	
 	this.get = function() {
-		var id = localStorageService.get('testingDemoAppStorage');
-		var url = 'https://dev.hel.fi:443/paatokset/v1/agenda_item/' + id;
-		
-		var data = {
-		        "name": "Send email to Adriana",
-		        "description": "Confirm if you have passed the subject",
-		        "done": true
-		};
 		
 		var deferred = $q.defer();
 		
 		$timeout(function() {
 			deferred.notify('get started');
-			$http.get(url).then(function(response) {
-	    		data.name = response.data.introducer;
-	    		data.description = response.data.subject;
-	    		deferred.resolve(data);
-	    	}, function(error) {
-	    		deferred.reject(error);
-	    	});
-		}, 0);
+			
+			$http.get('response2.json').then(function(response) {
+				deferred.resolve(response.data);
+            });
+		}, 3000);
 		
 		return deferred.promise;
 	}
