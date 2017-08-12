9a1a13048b64cd93ea0af76378f80b3ab5ff7bf6
Hannu Honka <hannu.honka@tieto.com>
2017-08-12 05:22:12 +0300

    Antin jsonforms ja bootstrap lisätty

--- demo/src/main/resources/static/js/task.uischema.js
+++ demo/src/main/resources/static/js/task.uischema.js
@@ -1,48 +1,59 @@
 angular.module('demoApp')
 	.value("UISchema",
 		{
-			"type" : "Group",
-			"label" : "Hakemuksen tiedot",
+			"type" : "Categorization",
 			"elements" : [
 				{
-					"type" : "HorizontalLayout",
-					"elements" : [
+					"type": "Category",
+					"label" : "Hakemuksen päivitystiedot",
+					"elements": [
 						{
-							"type" : "Control",
-							"label" : "Hakemustyyppi",
-							"scope" : {
-								"$ref" : "#/properties/Hakemustyyppi"
-							},
-							"readOnly" : true
-						}
-					]
-				},
-				{
-					"type" : "HorizontalLayout",
-					"elements" : [
-						{
-							"type" : "Control",
-							"label" : "Saapunut",
-							"scope" : {
-								"$ref" : "#/properties/Saapunut"
-							},
-							"readOnly" : true
-						}
-					]
-				},
-				{
-					"type" : "HorizontalLayout",
-					"elements" : [
-						{
-							"type" : "Control",
-							"label" : "Päivitetty",
-							"scope" : {
-								"$ref" : "#/properties/Päivitetty"
-							},
-							"readOnly" : true
+							"type": "Group",
+							"label" : "Hakemuksen tiedot",
+							"elements": [
+								{
+									"type": "HorizontalLayout",
+									"elements": [
+										{
+											"type" : "Control",
+											"label" : "Hakemustyyppi",
+											"scope" : {
+												"$ref" : "#/properties/Hakemustyyppi"
+											},
+											"readOnly": true
+										}
+									]
+								},
+								{
+									"type" : "HorizontalLayout",
+									"elements": [
+										{
+											"type" : "Control",
+											"label" : "Saapunut",
+											"scope" : {
+												"$ref" : "#/properties/Saapunut"
+											},
+											"readOnly" : true
+										}
+									]
+								},
+								{
+									"type" : "HorizontalLayout",
+									"elements": [
+										{
+											"type" : "Control",
+											"label" : "Päivitetty",
+											"scope" : {
+												"$ref" : "#/properties/Päivitetty"
+											},
+											"readOnly" : true
+										}
+									]
+								}
+								
+							]
 						}
 					]
 				}
-
 			]
 		});
