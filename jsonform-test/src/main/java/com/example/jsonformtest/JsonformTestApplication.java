9ebd6fcf112ff6ddc500e2f517eedc84bc516e80
Hannu Honka <hannu.honka@tieto.com>
2017-08-12 07:53:51 +0300

    jsonforms-test

---
+++ jsonform-test/src/main/java/com/example/jsonformtest/JsonformTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.jsonformtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonformTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonformTestApplication.class, args);
+	}
+}
