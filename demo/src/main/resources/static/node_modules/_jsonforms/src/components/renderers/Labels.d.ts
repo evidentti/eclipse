fef3cb285e79809c63e3b62c390a5a940a3216a8
Hannu Honka <hannu.honka@tieto.com>
2017-08-13 17:17:55 +0300

    “backup”

--- demo/src/main/resources/static/node_modules/_jsonforms/src/components/renderers/Labels.d.ts
+++
@@ -1,6 +0,0 @@
-import { ILabelObject, IWithLabel } from '../../uischema';
-export declare class Labels {
-    shouldShowLabel(withLabel: IWithLabel): boolean;
-    getElementLabelObject(withLabel: IWithLabel, schemaPath: string): ILabelObject;
-}
-export declare const LabelObjectUtil: Labels;
